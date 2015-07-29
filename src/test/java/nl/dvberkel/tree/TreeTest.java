4004e1a709bedeb72a3de566fd818c3ad2824465
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-29 07:55:41 +0200

    Prevent A node to be created with null sub trees

--- src/test/java/nl/dvberkel/tree/TreeTest.java
+++ src/test/java/nl/dvberkel/tree/TreeTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 
 public class TreeTest {
     @Test
@@ -11,6 +12,28 @@ public class TreeTest {
         Tree tree = new Leaf();
 
         assertThat(tree, is(not(nullValue())));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldNotAllowANodeWithLeftSubtreeNull(){
+        Tree aSubTree = mock(Tree.class);
+
+        Tree node = new Node(null, aSubTree);
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void shouldNotAllowANodeWithRightSubtreeNull(){
+        Tree aSubTree = mock(Tree.class);
+
+        Tree node = new Node(aSubTree, null);
+    }
+}
+
+class Node implements Tree {
+
+    public Node(Tree left, Tree right) {
+        if (left  == null) { throw new IllegalArgumentException(String.format("%s sub-tree is null", "left")); }
+        if (right == null) { throw new IllegalArgumentException(String.format("%s sub-tree is null", "right")); }
     }
 }
 
