60301324a98ad04de41fee44fdc5df13d5e3faf9
dvberkel <daan.v.berkel.1980@gmail.com>
2015-07-29 07:57:44 +0200

    Move Node to its own file

---
+++ src/main/java/nl/dvberkel/tree/Node.java
@@ -0,0 +1,9 @@
+package nl.dvberkel.tree;
+
+class Node implements Tree {
+
+    public Node(Tree left, Tree right) {
+        if (left  == null) { throw new IllegalArgumentException(String.format("%s sub-tree is null", "left")); }
+        if (right == null) { throw new IllegalArgumentException(String.format("%s sub-tree is null", "right")); }
+    }
+}
